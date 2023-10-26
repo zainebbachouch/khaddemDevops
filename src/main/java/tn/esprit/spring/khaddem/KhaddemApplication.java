0b04f44893b4654722e55c1d2caa4cbf1efd344c
zaineb.bachouch <zaineb.bachouch@esprit.tn>
2023-10-26 23:59:19 +0100

    16

--- src/main/java/tn/esprit/spring/khaddem/KhaddemApplication.java
+++ src/main/java/tn/esprit/spring/khaddem/KhaddemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class KhaddemApplication {
-//slm
+//hhhhhhhh
     public static void main(String[] args) {
         SpringApplication.run(KhaddemApplication.class, args);
     }
